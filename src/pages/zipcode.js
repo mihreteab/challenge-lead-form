2527dbed9becffa94234cacd96feeafa23497a58
mihreteab <mihreteabdemeke@gmail.com>
2021-10-15 14:57:41 +0300

    feat: add validation for zipcode page

--- src/pages/zipcode.js
+++ src/pages/zipcode.js
@@ -5,11 +5,37 @@ import {
   QuestionContainer,
   Wrapper
 } from '../components/styled-components'
+import styled from 'styled-components'
+import { useLeadContext } from '../state/context'
+import { useLeadFormUtils } from '../utils/useLeadFormUtils'
 import ContinueButton from '../components/continue-button'
 
+const InputContainer = styled.div`
+  width: inherit;
+  position: relative;
+  text-align: center;
+  ${({ hasError }) =>
+    hasError &&
+    `
+    input {
+      border: 1px solid red;
+    }
+    p {
+      font-size: 12px;
+      font-family: Montserrat;
+      color: red;
+      margin: 0px;
+    }
+  `}
+`
 
 export default function ZipCode() {
-  
+  const { state } = useLeadContext()
+  const { changeValue, addError, nextStep } = useLeadFormUtils()
+  const validateAndMove = () => {
+    if (!state.zipCode) return addError('zipCode', 'ZipCode is required')
+    nextStep()
+  }
   return (
     <Wrapper>
       <QuestionContainer isFullWidth>
@@ -19,14 +45,19 @@ export default function ZipCode() {
         Enter your property's <strong>zip code</strong>
       </DescriptionContainer>
       <InputsGroup isAddress>
-        <input
-          type="text"
-          name="zipCode"
-          id="zipCode"
-          placeholder="Enter zip code..."
-        />
+        <InputContainer hasError={state.errors.zipCode}>
+          <input
+            type='text'
+            name='zipCode'
+            id='zipCode'
+            value={state.zipCode}
+            onChange={e => changeValue('zipCode', e.target.value)}
+            placeholder='Enter zip code...'
+          />
+          {state.errors.zipCode && <p>{state.errors.zipCode}</p>}
+        </InputContainer>
       </InputsGroup>
-      <ContinueButton />
+      <ContinueButton onClick={validateAndMove} />
     </Wrapper>
   )
 }
